7289762c7b325bb9122e7fa66bc8e2b5dc9fe877
Thu Nguyen <thunguy@me.com>
2020-05-11 17:55:29 -0700

    add staticmethod, classmethod, & instance methods for class Cupcake

--- desserts.py
+++ desserts.py
@@ -7,15 +7,48 @@ class Cupcake:
     """cache stores all cupcake instances by name"""
     cache = {}
     
-    def __init__(self, name, qty):
+    def __init__(self, name, flavor, price):
         self.name = name
-        self.qty = qty
+        self.flavor = flavor
+        self.price = price
+        self.qty = 0
+        self.cache[name] = self
 
     def __repr__(self):
         """Human-readable printout for debugging."""
-
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
+    def add_stock(self, amount):
+        """Add amount to self.qty"""
+        self.qty += amount
+
+    def sell(self, amount):
+        """Sell the given amount of cupcakes and update self.qty."""
+        
+        if self.qty == 0:
+            print('Sorry, these cupcakes are sold out')
+            return
+
+        if self.qty < amount:
+            self.qty = 0
+            return
+
+        self.qty -= amount
+
+    @staticmethod
+    def scale_recipe(ingredients, amount): 
+        """Scale the list of ingredients by the given amount of cupcakes."""
+        return [(ingredient, qty * amount) for ingredient, qty in ingredients]
+
+    @classmethod
+    def get(cls, name):
+        """Return a cupcake from cls.cache if name exists"""
+        if name not in cls.cache:
+            return print("Sorry, that cupcake doesn't exist")
+            
+        
+        return cls.cache[name]
+        
 
 if __name__ == '__main__':
     import doctest
